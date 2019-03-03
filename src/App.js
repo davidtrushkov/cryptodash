3178547c6d81b0c8b9048950425967e0e0be4e66
hydrogen11 <davidisback4good@hotmail.com>
2019-03-03 18:25:26 -0500

    Inital Project Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to CryptoDash
+            Welcome to CryptoDash Davids
           </p>
           <a
             className="App-link"
