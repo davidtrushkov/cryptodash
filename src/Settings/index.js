248d4ca71a3bf047a2246d9865ebf21e8ca575b6
hydrogen11 <davidisback4good@hotmail.com>
2019-03-03 21:28:59 -0500

    Settings page, localStorage

---
+++ src/Settings/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import WelcomeMessage from '../Settings/WelcomeMessage';
+import ConfirmButton from './ConfirmButton';
+
+export default function Welcome() {
+    return <div><WelcomeMessage /> <ConfirmButton /></div>
+  }
