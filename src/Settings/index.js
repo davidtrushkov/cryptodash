142bf2850998fca119a70285820dfb81641ebfd0
hydrogen11 <davidisback4good@hotmail.com>
2019-03-06 02:33:10 -0500

    Add search

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,12 +3,14 @@ import WelcomeMessage from '../Settings/WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
+import Search from './Search';
 
 export default function Welcome() {
     return <Page name="settings">
       <WelcomeMessage />
       <CoinGrid topSection />
-      <ConfirmButton />
+        <ConfirmButton />
+        <Search />
       <CoinGrid />
       </Page>
   }
